19891ad408bcd165f78501e352ff21aee0e41d56
Gabriel Camurça <gabriel@pyxys.com.br>
2022-02-15 22:16:56 -0300

    test

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ const app = express();
 const PORT = process.env.PORT || 8877;
 
 app.get('/', (req, res) => {
-  res.json({
-    msg: "OK"
-  })
+  res.json([
+    {
+      name: "Alguma coisa",
+      description: "Alguma coisa qualquer",
+      popularity: "99",
+      images: [
+        {
+          url: 'https://aaaa',
+          alt: 'Alt'
+        },
+        {
+          url: 'https://aaaa',
+          alt: 'Alt'
+        },
+        {
+          url: 'https://aaaa',
+          alt: 'Alt'
+        }
+      ]
+    }
+  ])
 })
 
 app.listen(PORT, () => {
