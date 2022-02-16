c4db925314da97d460c0ffa069e9fcb31e392539
Gabriel Camurça <gabriel@pyxys.com.br>
2022-02-15 23:47:20 -0300

    add

--- index.js
+++ index.js
@@ -15,29 +15,37 @@ app.use(function(req, res, next) {
   next();
 });
 
+let dataBase = [
+  {
+    name: "Alguma coisa",
+    description: "Alguma coisa qualquer",
+    popularity: "99",
+    images: [
+      {
+        url: 'https://aaaa',
+        alt: 'Alt'
+      },
+      {
+        url: 'https://aaaa',
+        alt: 'Alt'
+      },
+      {
+        url: 'https://aaaa',
+        alt: 'Alt'
+      }
+    ]
+  }
+]
+
 app.get('/api/products', (req, res) => {
-  res.json([
-    {
-      name: "Alguma coisa",
-      description: "Alguma coisa qualquer",
-      popularity: "99",
-      images: [
-        {
-          url: 'https://aaaa',
-          alt: 'Alt'
-        },
-        {
-          url: 'https://aaaa',
-          alt: 'Alt'
-        },
-        {
-          url: 'https://aaaa',
-          alt: 'Alt'
-        }
-      ]
-    }
-  ])
+  res.json(dataBase)
 })
+
+app.post('/api/product/create', function(req, res) {
+  const body = req.body
+  dataBase.push(body);
+  res.json(body)
+});
 
 app.listen(PORT, () => {
   console.log('Escutando na porta: ' + PORT)
