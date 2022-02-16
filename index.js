5eea9e864af6c60c4fb070a352ad687c792593d1
Gabriel Camurça <gabriel@pyxys.com.br>
2022-02-15 23:17:14 -0300

    add

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8877;
 
+app.use(function(req, res, next) {
+  // Set permissive CORS header - this allows this server to be used only as
+  // an API server in conjunction with something like webpack-dev-server.
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+
+  // Disable caching so we'll always get the latest comments.
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
 app.get('/api/products', (req, res) => {
   res.json([
     {
