916a460866be92a1936eabcf292843de0c571422
Gabriel Camurça <gabriel@pyxys.com.br>
2022-05-19 16:30:19 -0300

    add

--- index.js
+++ index.js
@@ -3,36 +3,29 @@ const app = express();
 const fs = require('fs')
 var cors = require('cors')
 var bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8877;
+var PRODUCTS_FILE = './save.json'
 
-app.use(cors({
+var corsConfig = {
   origin: ['http://localhost:3000', 'https://apipromofaster.vercel.app', 'https://promo-faster.herokuapp.com', 'http://promo-faster.herokuapp.com'], 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
-}))
+}
 
-const PORT = process.env.PORT || 8877;
-
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-var PRODUCTS_FILE = './save.json'
 
 app.use(function(req, res, next) {
   // Set permissive CORS header - this allows this server to be used only as
   // an API server in conjunction with something like webpack-dev-server.
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Acept, Origin, X-Request-Width');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Acept, Origin, X-Request-Width');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
 
   // Disable caching so we'll always get the latest comments.
   res.setHeader('Cache-Control', 'no-cache');
   next();
-});
-
-import dataBase from './save.json'
-
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
 });
 
 app.get('/api/products', (req, res) => {
