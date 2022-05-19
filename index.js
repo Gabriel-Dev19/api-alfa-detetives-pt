e7d3c77b9d38d590a6349c4aeb90928ca27baf46
Gabriel Camurça <gabriel@pyxys.com.br>
2022-05-19 19:38:03 -0300

    add

--- index.js
+++ index.js
@@ -21,34 +21,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var PRODUCTS_FILE = './save.json'
 
-// app.use(function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   // Disable caching so we'll always get the latest comments.
-//   //res.setHeader('Cache-Control', 'no-cache');
-//   next();
-// });
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-module.exports = allowCors()
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Disable caching so we'll always get the latest comments.
+  //res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
 
 import dataBase from './save.json'
 
