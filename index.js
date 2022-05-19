da2cbf4dd80489520a7515424fbeb5f1168cd964
Gabriel Camurça <gabriel@pyxys.com.br>
2022-05-19 19:13:00 -0300

    add

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var corsConfig = {
   origin: 'https://promo-faster.herokuapp.com',
   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
-  allowedHeaders: 'Access-Control-Allow-Headers, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Request-Method, Access-Control-Request-Headers',
+  allowedHeaders: 'Content-Type',
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 204
 }
