fd5cf3c082698e5044de162c25f1dc6ff2b9063e
Gabriel camurça <gabrielcamurcaaa10@gmail.com>
2022-10-12 14:31:47 -0300

    add

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ var cors = require('cors')
 var bodyParser = require('body-parser');
 
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
@@ -18,22 +16,10 @@ const PORT = process.env.PORT || 8877;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var PRODUCTS_FILE = './save.json'
+var DISTRITOS_FILE = './save.json'
 
-// app.use(function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   // Disable caching so we'll always get the latest comments.
-//   //res.setHeader('Cache-Control', 'no-cache');
-//   next();
-// });
-
-import dataBase from './save.json'
-
-app.get('/api/products', (req, res) => {
-  fs.readFile(PRODUCTS_FILE, function(err, data) {
+app.get('/api/distritos', (req, res) => {
+  fs.readFile(DISTRITOS_FILE, function(err, data) {
     if (err) {
         console.error(err);
         process.exit(1);
@@ -42,95 +28,31 @@ app.get('/api/products', (req, res) => {
   });
 })
 
-app.get('/api/product/:id', function(req, res) {
+app.get('/api/distritos/:distrito', function(req, res) {
 
-  fs.readFile(PRODUCTS_FILE, function(err, data) {
+  fs.readFile(DISTRITOS_FILE, function(err, data) {
       if (err) {
           console.error(err);
           process.exit(1);
       }
-
       var json = JSON.parse(data);
-
-      for(var i = 0; i <= json.length; i++)
-      {
-          if(json[i]['id'] == req.params.id)
-          {
-              res.send(json[i]);
-              break;
-          }
-      }
+      const distrito = json.find((item) => item.distrito === String(req.params.distrito))
+      res.send(distrito)
   });
 });
 
-app.post('/api/products/create', function(req, res) {
-  fs.readFile(PRODUCTS_FILE, function(err, data) {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
-    var products = JSON.parse(data);
+app.get('/api/distritos/:distrito/:cidade', function(req, res) {
 
-    var newProduct = {
-      id: Date.now(),
-      name: req.body.name,
-      description: req.body.description,
-      preco: req.body.preco,
-      popularity: req.body.popularity,
-      categorySearch: req.body.categorySearch,
-      precoAntigo: req.body.precoAntigo,
-      porcentagemDesconto: req.body.porcentagemDesconto,
-      numeroParcelas: req.body.numeroParcelas,
-      precoParcelas: req.body.precoParcelas,
-      semJuros: req.body.semJuros,
-      link: req.body.link,
-      images: req.body.images,
-      url: req.body.images.url,
-      alt: req.body.images.alt
-    };
-    products.push(newProduct);
-    fs.writeFile(PRODUCTS_FILE, JSON.stringify(products, null, 2), function(err) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        res.json(products);
-    });
-});
-});
-
-app.delete('/api/products/delete/:id', function(req, res) {
-  //for(var i = 0; i <= dataBase.length; i++)
-  //  {
-  //    if(dataBase[i]['id'] == req.params.id)
-  //    {
-  //      dataBase.splice(i, 1);
-  //      res.json(dataBase);
-  //      break;
-  //    }
-  //  }
-  fs.readFile(PRODUCTS_FILE, function (err, data) {
-    if (err) {
-      console.error(err)
-      process.exit(1)
-    }
-    const products = JSON.parse(data)
-
-    for (let i = 0; i <= products.length; i++) {
-      // eslint-disable-next-line eqeqeq
-      if (products[i].id == req.params.id) {
-        products.splice(i, 1)
-        fs.writeFile(PRODUCTS_FILE, JSON.stringify(products, null, 2), function (err) {
-          if (err) {
-            console.error(err)
-            process.exit(1)
-          }
-          res.json(products)
-        })
-        break
+  fs.readFile(DISTRITOS_FILE, function(err, data) {
+      if (err) {
+          console.error(err);
+          process.exit(1);
       }
-    }
-  })
+      var json = JSON.parse(data);
+      const distrito = json.find((item) => item.distrito === String(req.params.distrito))
+      const cidade = distrito.cidades.find((item) => item.cidade === String(req.params.cidade))
+      res.json(cidade)
+  });
 });
 
 app.listen(PORT, () => {
